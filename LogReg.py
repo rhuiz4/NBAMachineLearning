aab57a085b3361e9d9e45a02e76a6eb8eb5816da
Rihui Zheng <rz1276@nyu.edu>
2021-04-23 20:22:10 -0400

    update csv reading

--- LogReg.py
+++ LogReg.py
@@ -4,7 +4,19 @@ import numpy as np
 import pandas as pd
 import sklearn
 
-play_data = pd.read_csv("player_data.csv")
+
+STATS_CSV = "Seasons_Stats.csv"
+PLAYER_DATA_CSV = "player_data.csv"
+
+
+play_data = pd.read_csv(PLAYER_DATA_CSV)
+print(play_data.shape)
+
+play_data1 = play_data.dropna(0)
+print(play_data1.shape)
+
+
+stats = pd.read_csv(STATS_CSV)
 print(play_data.shape)
 
 play_data1 = play_data.dropna(0)
